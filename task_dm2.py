7a5a93c2c1f0dc19eedf472f04ab6d48e7366625
Елена <ele69@rambler.ru>
2019-11-04 17:17:36 +0300

    Нарисовала координатную сетку

--- task_dm2.py
+++ task_dm2.py
@@ -1,9 +1,28 @@
 from drawman import *
 from time import sleep
 
+def coordinate_grid(n):
+    """"""
+    to_point(-300//n, 300//n)
+    for i in range(0, 600//n+1, 100//n):
+        pen_down()
+        on_vector(0, -600//n)
+        pen_up()
+        on_vector(100//n, 600//n)
+    to_point(-300//n, 300//n)
+    for i in range(0, 600//n, 100//n):
+        pen_down()
+        on_vector(600//n, 0)
+        pen_up()
+        on_vector(-600//n, -100//n)
+    to_point(0, 0)
+
 def f(x):
     return x*x
-drawman_scale(100)
+
+n=20
+drawman_scale(n)
+coordinate_grid(n)
 x=-5.0
 to_point(x, f(x))
 pen_down()
